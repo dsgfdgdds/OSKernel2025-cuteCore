5121b5b33ea7b4e482a4a80ffaf7dd16b1ed2cf7
pillow <waysorrypub@gmail.com>
2026-01-02 17:22:48 +0800

    删除task/mod中未使用的use

--- os/src/task/mod.rs
+++ os/src/task/mod.rs
@@ -11,9 +11,8 @@ use alloc::vec::Vec;
 pub use context::TaskContext;
 use lazy_static::lazy_static;
 pub use manager::{add_task, pid2process, remove_from_pid2process, wakeup_task};
-pub use pid::{pid_alloc, PidHandle};
 pub use processor::{
-    current_kstack_top, current_process, current_task, current_trap_cx, current_trap_cx_user_va,
+    current_process, current_task, current_trap_cx, current_trap_cx_user_va,
     current_user_token, run_tasks, schedule, take_current_task,
 };
 
